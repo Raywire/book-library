03555e1021017818a81be0785048c4331b4c91a1
Raywire <ryanwire@outlook.com>
2019-08-08 22:59:09 +0300

    Initial Setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('tiny')); // you can use 'combined'
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/views', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+app.get('/', (req, res) => {
+  res.render('index', { title: 'library' });
+});
+
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
+});
