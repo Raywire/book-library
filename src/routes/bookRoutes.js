00f9061f6c334a4dd8475e5e3fbf5fae8625be8c
Raywire <ryanwire@outlook.com>
2019-09-03 18:17:38 +0300

    Adds input parameter

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,64 +5,18 @@ const debug = require('debug')('app:bookRoutes');
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [
-    {
-      title: 'War and Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false,
-    },
-    {
-      title: 'Les Miserables',
-      genre: 'Historical Fiction',
-      author: 'Victor Hugo',
-      read: false,
-    },
-    {
-      title: 'The Time Machine',
-      genre: 'Science Fiction',
-      author: 'H. G. Wells',
-      read: false,
-    },
-    {
-      title: 'A Journey into the Center of The Earth',
-      genre: 'Science Fiction',
-      author: 'Jules Verne',
-      read: false,
-    },
-    {
-      title: 'The Dark World',
-      genre: 'Fantasy',
-      author: 'Henry Kuttner',
-      read: false,
-    },
-    {
-      title: 'The Wind in the Willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Grahame',
-      read: false,
-    },
-    {
-      title: 'Life on the Mississippi',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: false,
-    },
-
-  ];
-
   bookRouter.route('/')
     .get((req, res) => {
       (async function query() {
         const request = new sql.Request();
-        const result = await request.query('select * from books');
-        debug(result);
+        const { recordset } = await request.query('select * from books');
+        debug(recordset);
         res.render(
           'bookListView',
           {
             nav,
             title: 'Library',
-            books: result.recordset
+            books: recordset
           }
         );
       }());
@@ -70,15 +24,23 @@ function router(nav) {
 
   bookRouter.route('/:id')
     .get((req, res) => {
-      const { id } = req.params;
-      res.render(
-        'bookView',
-        {
-          nav,
-          title: 'Library',
-          book: books[id]
-        }
-      );
+      (async function query() {
+        const { id } = req.params;
+        const request = new sql.Request();
+        const { recordset } = await request
+          .input('id', sql.Int, id)
+          .query('select * from books where id=@id');
+
+        debug(recordset);
+        res.render(
+          'bookView',
+          {
+            nav,
+            title: 'Library',
+            book: recordset[0]
+          }
+        );
+      }());
     });
   return bookRouter;
 }
