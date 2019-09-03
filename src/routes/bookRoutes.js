830ebac80cfa7c96ee6a693b6412df84c88a9cf0
Raywire <ryanwire@outlook.com>
2019-09-03 14:36:35 +0300

    Adds Routing -Sets up navigation -Sets up routing -Sets up rendering

---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const bookRouter = express.Router();
+
+function router(nav) {
+  const books = [
+    {
+      title: 'War and Peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false,
+    },
+    {
+      title: 'Les Miserables',
+      genre: 'Historical Fiction',
+      author: 'Victor Hugo',
+      read: false,
+    },
+    {
+      title: 'The Time Machine',
+      genre: 'Science Fiction',
+      author: 'H. G. Wells',
+      read: false,
+    },
+    {
+      title: 'A Journey into the Center of The Earth',
+      genre: 'Science Fiction',
+      author: 'Jules Verne',
+      read: false,
+    },
+    {
+      title: 'The Dark World',
+      genre: 'Fantasy',
+      author: 'Henry Kuttner',
+      read: false,
+    },
+    {
+      title: 'The Wind in the Willows',
+      genre: 'Fantasy',
+      author: 'Kenneth Grahame',
+      read: false,
+    },
+    {
+      title: 'Life on the Mississippi',
+      genre: 'History',
+      author: 'Mark Twain',
+      read: false,
+    },
+
+  ];
+
+  bookRouter.route('/')
+    .get((req, res) => {
+      res.render(
+        'bookListView',
+        {
+          nav,
+          title: 'Library',
+          books
+        }
+      );
+    });
+
+  bookRouter.route('/:id')
+    .get((req, res) => {
+      const { id } = req.params;
+      res.render(
+        'bookView',
+        {
+          nav,
+          title: 'Library',
+          book: books[id]
+        }
+      );
+    });
+  return bookRouter;
+}
+
+module.exports = router;
