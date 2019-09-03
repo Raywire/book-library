fd283d7f51d8ae2b242e9d6d4dd76015a49dcae1
Raywire <ryanwire@outlook.com>
2019-09-03 20:26:56 +0300

    Adds adminRouter

--- app.js
+++ app.js
@@ -3,23 +3,10 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const sql = require('mssql');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.SERVER,
-  database: process.env.DATABASE,
-  options: {
-    encrypt: true
-  }
-};
-
-sql.connect(config).catch(err => debug(err));
 
 app.use(morgan('tiny')); // you can use 'combined'
 app.use((req, res, next) => {
@@ -35,8 +22,10 @@ app.set('view engine', 'ejs');
 
 const nav = [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }];
 const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
 app.get('/', (req, res) => {
   res.render(
     'index',
