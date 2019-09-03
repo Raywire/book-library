df889b0f754f93acfedc3c2adb17b2c66e9bca0b
Raywire <ryanwire@outlook.com>
2019-09-03 17:51:59 +0300

    Connects to MSSQL

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.SERVER,
+  database: process.env.DATABASE,
+  options: {
+    encrypt: true
+  }
+};
+
+sql.connect(config).catch(err => debug(err));
 
 app.use(morgan('tiny')); // you can use 'combined'
 app.use(express.static(path.join(__dirname, 'public')));
