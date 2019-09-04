8b13e4661192188eed71c6c9ad0f4ceafa3ddb53
Raywire <ryanwire@outlook.com>
2019-09-04 21:16:27 +0300

    Adds authentication - Set up sign up - Set up authRoutes - Set up Passport - Set up Local Strategy - Creates user - Log in user - Validates user - Authorizes user

---
+++ README.md
@@ -0,0 +1,41 @@
+## Title
+Library
+
+## Getting Started
+
+git clone the repo
+
+### Prerequisites
+
+A mongo database is required
+
+**Setting up the database**
+```
+Navigate to /admin to populate books
+```
+
+## Running the app
+cd into the book-library folder
+Install the dependencies
+```node
+npm install
+```
+Run the app in development
+```node
+npm run dev
+```
+
+## Built With
+
+*   [NodeJS](https://nodejs.org/) - NodeJS
+*   [ExpressJS](https://expressjs.com/) - ExpressJS
+*   [MongoDB](https://docs.mongodb.com/) - MongoDB
+
+
+## Author
+
+*   **Ryan Simiyu** 
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny')); // you can use 'combined'
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'library' }));
+
+require('./src/config/passport.js')(app);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
@@ -19,9 +30,13 @@ app.set('view engine', 'ejs');
 const nav = [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }];
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
+
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
+
 app.get('/', (req, res) => {
   res.render(
     'index',
