0151641012bf640e8c84649e13a1a10b8d8c7309
Raywire <ryanwire@outlook.com>
2019-09-11 13:39:15 +0300

    Adds a books controller

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,86 +1,16 @@
 const express = require('express');
-const { MongoClient, ObjectID } = require('mongodb');
-const debug = require('debug')('app:bookRoutes');
+const bookController = require('../controllers/bookController');
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  bookRouter.use((req, res, next) => {
-    // You can add req.user.admin etc etc
-    if (req.user) {
-      next();
-    } else {
-      res.redirect('/');
-    }
-  });
+  const { getIndex, getById, authMiddleware } = bookController(nav);
+  bookRouter.use(authMiddleware);
   bookRouter.route('/')
-    .get((req, res) => {
-      const url = 'mongodb://localhost:27017';
-      const dbName = 'libraryApp';
-
-      (async function mongo() {
-        let client;
-        try {
-          client = await MongoClient.connect(url, {
-            useNewUrlParser: true, useUnifiedTopology: true
-          });
-          debug('connected correctly to server');
-
-          const db = client.db(dbName);
-          const col = await db.collection('books');
-
-          const books = await col.find().toArray();
-          res.render(
-            'bookListView',
-            {
-              nav,
-              title: 'Library',
-              books
-            }
-          );
-        } catch (err) {
-          debug(err.stack);
-        }
-        client.close();
-      }());
-    });
+    .get(getIndex);
 
   bookRouter.route('/:id')
-    .get((req, res) => {
-      const { id } = req.params;
-      const url = 'mongodb://localhost:27017';
-      const dbName = 'libraryApp';
-
-      (async function mongo() {
-        let client;
-        try {
-          client = await MongoClient.connect(url, {
-            useNewUrlParser: true, useUnifiedTopology: true
-          });
-          debug('connected correctly to server');
-
-          const db = client.db(dbName);
-          const col = await db.collection('books');
-
-          const book = await col.findOne({
-            _id: new ObjectID(id)
-          });
-
-          debug(book);
-          res.render(
-            'bookView',
-            {
-              nav,
-              title: 'Library',
-              book
-            }
-          );
-        } catch (err) {
-          debug(err.stack);
-        }
-        client.close();
-      }());
-    });
+    .get(getById);
   return bookRouter;
 }
 
